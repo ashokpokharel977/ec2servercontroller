fb28403d09f7aa6936d74b1bd8e22663141c230e
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-12 11:25:57 +0545

    get instances

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ var ec2parameters = {
 ec2.describeInstances(ec2parameters, function (err, data) {
 	if (err) console.log(err, err.stack);
 	else {
-		console.log(data.Reservations);
+		console.log(data.Reservations[0].Instances);
 	}
 });
