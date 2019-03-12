a161b29fa94ee73439942117a421cfba9e7d9762
ashok pokharel <ashokpokharel977@gmail.com>
2019-03-12 11:23:59 +0545

    commit

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ var ec2parameters = {
 ec2.describeInstances(ec2parameters, function (err, data) {
 	if (err) console.log(err, err.stack);
 	else {
-		console.log(data);
+		console.log(data.Reservations);
 	}
 });
